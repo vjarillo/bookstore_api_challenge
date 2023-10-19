03461803af5e2fc762131a87147c52dc1ede83ce
David Jarillo <david.jarillo@michelada.io>
2023-10-18 21:27:05 -0700

    adding validation to avoid assign same books to an author, and adding option to destroy books from an author

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,6 @@ class Author < ApplicationRecord
   validates_presence_of :first_name, :last_name, :date_of_birth
   validates_uniqueness_of :first_name, :last_name
 
-  has_many :published_books
+  has_many :published_books, dependent: :destroy
   has_many :books, through: :published_books
 end
--- app/models/published_book.rb
+++ app/models/published_book.rb
@@ -3,4 +3,8 @@
 class PublishedBook < ApplicationRecord
   belongs_to :author
   belongs_to :book
+
+  validates :author_id, uniqueness: {
+    scope: [:book_id]
+  }
 end

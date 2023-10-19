9c6ba614ea99d10b24959d29882a9c48d10d464c
David Jarillo <david.jarillo@michelada.io>
2023-10-19 14:50:23 -0700

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@
 
 Rails.application.routes.draw do
   resources :authors do
-    member  do
+    member do
       get 'books', to: 'authors#list_books'
       post 'add_book/:book_id', to: 'authors#add_book'
       delete 'remove_book/:book_id', to: 'authors#remove_book'
     end
   end
   resources :books
+
+  get '/home', to: ->(_env) { [200, { 'Content-Type' => 'application/json' }, [{ message: 'Home Page!' }.to_json]] }
 end
